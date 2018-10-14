f8ba40879ba89e087817bf3b01cc7560e110fe44
Grant Maki <maki.gr@gmail.com>
2018-10-14 14:53:51 -0400

    exercise 1.10

--- ch1/exercises-1-2.scm
+++ ch1/exercises-1-2.scm
@@ -28,4 +28,45 @@
 ;(+ 0 6)
 ;6
 
+;Exercise 1.10
+(define (A x y)
+  (cond ((= y 0) 0)
+		((= x 0) (* 2 y))
+		((= y 1) 2)
+		(else (A (- x 1) (A x (- y 1))))
+		)
+  )
 
+(a 1 10)
+;(a 0 (a 1 9))
+;(a 0 (a 0 (a 1 8)))
+; ... 9x nested until (a 1 1) , = 2^10
+;1024
+(a 2 4)
+;(a 1 (a 2 3))
+;(a 1 (a 1 (a 2 2)))
+;(a 1 (a 1 (a 1 (a 2 1)))) 
+;(a 1 (a 1 (a 1 2)))
+;(a 1 (a 1 (a 0 (a 1 1))))
+;(a 1 (a 1 (a 0 2)))
+;(a 1 (a 1 4))
+;(a 1 (a 0 (a 1 3)))
+;(a 1 (a 0 (a 0 (a 1 2)))
+;(a 1 (a 0 (a 0 (a 0 (a 1 1)))
+;(a 1 (a 0 (a 0 (a 0 2))
+;(a 1 (a 0 (a 0 4)
+;(a 1 (a 0 8))
+;(a 1 16)
+;pattern recognition says (a 1 x) = 2 ^ x
+;65536
+(a 3 3)
+;just gonna evaluate this one, thanks
+;65536
+
+;What is a concise definition of f, g, and h?
+;(define (f n) (a 0 n))
+;2n
+;(define (g n) (a 1 n))
+;2^n
+;(define (h n (a 2 n)))
+;2^(2^n)
