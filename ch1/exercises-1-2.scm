23f3e9db1b485816166f0fed4ddf19599d10dc72
Grant Maki <maki.gr@gmail.com>
2018-10-26 17:47:01 -0400

    exercise 1.21

--- ch1/exercises-1-2.scm
+++ ch1/exercises-1-2.scm
@@ -224,3 +224,25 @@
 ;if (= 0 0) 2
 ;termiante
 ;4 evals
+
+
+;Exercise 1.21
+(define (smallest-divisor n)
+  (find-divisor n 2))
+
+(define (find-divisor n test-divisor)
+  (cond ( (> (square test-divisor) n) n)
+		((divides? test-divisor n) test-divisor)
+		(else (find-divisor n (+ test-divisor 1)))
+		)
+  )
+(define (divides? a b)
+  (= (remainder b a) 0)
+  )
+
+(smallest-divisor 199)
+;199
+(smallest-divisor 1999)
+;1999
+(smallest-divisor 19999)
+;7
